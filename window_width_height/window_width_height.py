d82281bbf2a152bcdb8be213a2d314a4c4824689
MohammadReza Omrani <52138433+mohammad-reza-omrani@users.noreply.github.com>
2025-03-02 22:30:52 +0100

    Create window_width_height.py

---
+++ window_width_height/window_width_height.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+# Create the main window
+mainWindow = tk.Tk()
+mainWindow.title("Coordinate")
+
+# Get screen width and height
+screen_width = mainWindow.winfo_screenwidth()
+screen_height = mainWindow.winfo_screenheight()
+
+# Get window dimensions
+mainWindow_width = 500
+mainWindow_height = 500
+
+# Start the Tkinter event loop
+mainWindow.mainloop()
